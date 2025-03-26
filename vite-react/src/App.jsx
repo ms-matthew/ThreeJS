e2d822fb55d0874ea0e89132de4e50878b6f9e0c
ms-matthew <pcmaster123321@gmail.com>
2025-03-26 18:00:14 +0100

    Last update on this test site

--- vite-react/src/App.jsx
+++ vite-react/src/App.jsx
@@ -1,4 +1,4 @@
-import {Cylinder, OrbitControls} from "@react-three/drei";
+import {Cylinder, OrbitControls, Sparkles} from "@react-three/drei";
 import {Canvas, useFrame} from "@react-three/fiber";
 import {useRef} from "react";
 
@@ -14,8 +14,10 @@ const RotatingCube = () => {
 
     return (
         <mesh ref={meshRef}>
-            <CylinderGeometry args={[1, 1, 1]} />
+            <cylinderGeometry args={[1, 1, 1]} />
             <meshLambertMaterial color="#468585" emissive="#468585" />
+
+            <Sparkles count={100} scale={1} size={6} speed={0.002} noise={0.02} color="orange" />
         </mesh>
     )
 }
@@ -29,7 +31,7 @@ const App = () => {
 
             <color attach="background" args={['#F0F0F0']} />
 
-            <RotationgCube/>
+            <RotatingCube/>
         </Canvas>
     )
 }
